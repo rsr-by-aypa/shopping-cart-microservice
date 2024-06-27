6a313d6c39ae8eea2e73e986750fd770aa5feed5
Grischakrombacha <grischa.storch@gmail.com>
2024-06-27 15:27:03 +0200

    Initial Commit

---
+++ src/main/java/com/rsr/shopping_cart_microservice/ShoppingCartMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.rsr.shopping_cart_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rsr/shopping_cart_microservice/ShoppingCartMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsr.shopping_cart_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
